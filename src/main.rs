e8615b0a418edc2fcaf638f41c4b4f3021c7fec5
bartoj35 <barton.jakub1811@gmail.com>
2022-02-13 14:48:55 +0100

    Split parsing and run into separate files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use std::env;
+use std::process;
+
+
 
 fn main() {
 	let arguments: Vec<String> = env::args().collect();
-	println!("{:?}", arguments);
+	let config = Config::new(&arguments)
+		.unwrap_or_else(|error| {
+			println!("Problem parsing arguments: {}", error);
+			process::exit(1);
+		});
+
+	println!("Searching for: {}", config.query);
+	println!("In file: {}", config.filename);
+	
+	if let Err(error) = run(config) {
+		println!("Application error: {}", error);
+		process::exit(1);
+	}
 }
+
